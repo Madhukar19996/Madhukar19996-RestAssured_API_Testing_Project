5e5f659f8ce9685528185fc62773fbf2df83d9e5
Madhukar <madhukar19996@gmail.com>
2025-06-23 09:45:57 +0530

    Fourth Commit on 23-June-2025

---
+++ src/test/java/day4/FileUploadAndDownload.java
@@ -0,0 +1,72 @@
+package day4;
+import static org.hamcrest.Matchers.*;
+
+import java.io.File;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+public class FileUploadAndDownload {
+	
+	//1) Single file upload 
+	
+	//@Test()
+	
+	void uploadSingleFile()
+	{
+		File myfile=new File("C:\\Users\\Madhukar Pandey\\Desktop\\students.json");
+		given()
+		     .multiPart("file",myfile)
+		     .contentType("multipart/form-data")
+		
+		.when()
+		     .post("http://localhost:8080/uploadFile")
+		
+		
+		.then()
+		     .statusCode(200)
+		     .body("fileName", equalTo("students.json"))
+		     .log().body();
+	}
+	
+	//2) Multiple files upload 
+	
+		//@Test()
+		
+		void multipleFilesUpload()
+		{
+			File myfile1=new File("C:\\Users\\Madhukar Pandey\\Desktop\\students.json");
+			File myfile2=new File("C:\\Users\\Madhukar Pandey\\Desktop\\csvjson.json");
+			
+			given()
+			     .multiPart("files",myfile1)
+			     .multiPart("files",myfile2)
+			     .contentType("multipart/form-data")
+			
+			.when()
+			     .post("http://localhost:8080/uploadMultipleFiles")
+			
+			
+			.then()
+			     .statusCode(200)
+			     .body("[0].fileName", equalTo("students.json"))
+			     .body("[1].fileName", equalTo("csvjson.json"))
+			     .log().body();
+		}
+		
+		@Test()
+		void downloadfile()
+		{
+			given()
+			     .pathParam("filename", "students.json")
+			     
+			
+			.when()
+			     .get("http://localhost:8080/downloadFile/{filename}")
+			.then()
+			      .statusCode(200)
+			      .log().body();
+		}
+
+
+}
