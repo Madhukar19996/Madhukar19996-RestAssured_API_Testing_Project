52f5ff4d39f71dbba109cbd041792b52761f0d30
Madhukar <madhukar19996@gmail.com>
2025-07-09 12:09:27 +0530

    Second commit on 9-July-2025

--- src/test/java/day9/RequestAndResponseSpecification.java
+++ src/test/java/day9/RequestAndResponseSpecification.java
@@ -12,39 +12,39 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
 public class RequestAndResponseSpecification {
-	
+
 	@Test(priority=1)
-	
+
 	void getAllEmployess()
 	{
 		given()
-		
+
 		.when()
-		     .get("http://localhost:3000/employees")
-		
+		.get("http://localhost:3000/employees")
+
 		.then()
-		     .statusCode(200)
-		     .log().body()
-		     .body("size()",greaterThan(0));
-	
+		.statusCode(200)
+		.log().body()
+		.body("size()",greaterThan(0));
+
 	}
-	
-	
-@Test(priority=2)
-	
+
+
+	@Test(priority=2)
+
 	void getMaleEmployess()
 	{
 		given()
-		    .queryParam("gender", "Male")
-		
+		.queryParam("gender", "Male")
+
 		.when()
-		     .get("http://localhost:3000/employees")
-		
+		.get("http://localhost:3000/employees")
+
 		.then()
-		     .statusCode(200)
-		     .log().body()
-		     .body("gender", everyItem(equalTo("Male")));
-	
+		.statusCode(200)
+		.log().body()
+		.body("gender", everyItem(equalTo("Male")));
+
 	}
 
 }
