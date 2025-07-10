15edc9a9c752047e6636d23df95e62c220698a4f
Madhukar <madhukar19996@gmail.com>
2025-07-10 09:12:33 +0530

    Adding files on 10-July-2025

--- src/test/java/day9/RequestAndResponseSpecification.java
+++ src/test/java/day9/RequestAndResponseSpecification.java
@@ -11,21 +11,64 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
+/*
+ 
+http://localhost:3000/employees
+http://localhost:3000/employees/1
+
+http://localhost:3000/employees?last_name=King
+http://localhost:3000/employees?first_name=Steven
+
+http://localhost:3000/employees?gender=Female
+http://localhost:3000/employees?gender=Male
+
+ 
+ */
+
+
+
 public class RequestAndResponseSpecification {
+	
+	
+	RequestSpecification httpRequest;
+	ResponseSpecification httpResponse;
+	
+	@BeforeClass
+	void setup() 
+	{
+		// Create  request specification Builder
+		
+		RequestSpecBuilder reqBuilder=new RequestSpecBuilder();
+		reqBuilder.setBaseUri("http://localhost:3000");
+		reqBuilder.setBasePath("employees");
+		
+		httpRequest=reqBuilder.build();
+		
+		//Create response specification Builder
+		ResponseSpecBuilder resBuilder=new ResponseSpecBuilder();
+		resBuilder.expectStatusCode(200);
+		resBuilder.expectHeader("Content-Type", equalTo("application/json"));
+		
+		
+		httpResponse=resBuilder.build();
+	
+
+	
+	}
 
 	@Test(priority=1)
 
 	void getAllEmployess()
 	{
 		given()
-
+             .spec(httpRequest)
 		.when()
-		.get("http://localhost:3000/employees")
+		     .get()
 
 		.then()
-		.statusCode(200)
-		.log().body()
-		.body("size()",greaterThan(0));
+		   .spec(httpResponse)
+		   .log().body()
+		   .body("size()",greaterThan(0));
 
 	}
 
@@ -35,15 +78,93 @@ public class RequestAndResponseSpecification {
 	void getMaleEmployess()
 	{
 		given()
-		.queryParam("gender", "Male")
+		   .spec(httpRequest)
+		   .queryParam("gender", "Male")
+		   
 
 		.when()
-		.get("http://localhost:3000/employees")
+		    .get()
 
 		.then()
-		.statusCode(200)
+		.spec(httpResponse)
 		.log().body()
 		.body("gender", everyItem(equalTo("Male")));
+
+	}
+	
+	
+	@Test(priority=4)
+
+	void getFirstname()
+	{
+		given()
+		.queryParam("first_name", "Steven")
+		.spec(httpRequest)
+
+		.when()
+		.get()
+
+		.then()
+		.spec(httpResponse)
+		.log().body()
+		.body("first_name", everyItem(equalTo("Steven")));
+
+	}
+	
+
+	@Test(priority=5)
+
+	void getLastname()
+	{
+		given()
+		.queryParam("last_name", "King")
+		.spec(httpRequest)
+
+		.when()
+		.get()
+
+		.then()
+		.spec(httpResponse)
+		.log().body()
+		.body("last_name", everyItem(equalTo("King")));
+
+	}
+	
+	
+	@Test(priority=6)
+
+	void getEmployeesbyId()
+	{
+		given()
+		
+		.spec(httpRequest)
+
+		.when()
+		.get("/1")
+
+		.then()
+		.spec(httpResponse)
+		.log().body()
+		.body("id",equalTo("1"));
+
+	}
+	
+	
+	@Test(priority=3)
+
+	void getFemaleEmployess()
+	{
+		given()
+		.queryParam("gender", "Female")
+		.spec(httpRequest)
+
+		.when()
+		.get()
+
+		.then()
+		.spec(httpResponse)
+		.log().body()
+		.body("gender", everyItem(equalTo("Female")));
 
 	}
 
